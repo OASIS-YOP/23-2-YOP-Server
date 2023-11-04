a170f16c69bfa6b080520c0390b1e7dd275b712c
urimJ <ufrl128@dgu.ac.kr>
2023-11-04 21:37:05 +0900

    MOD mainpage api

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -1,5 +1,11 @@
 
 /**
+ * 
+ * @swagger
+ * tags:
+ *  name: Mainpage
+ *  description: 메인페이지 API
+ * 
  * @swagger
  * paths:
  *  /mainpage/:nickname:
@@ -26,6 +32,35 @@
  *         type: string
  *     401:
  *      description: 닉네임 조회 실패
+ *      schema:
+ *       properties:
+ *        message:
+ *         type: string
+ *
+ *
+ * @swagger
+ * paths:
+ *  /mainpage:
+ *   get:
+ *    summary: 폴꾸 hot10
+ *    tags: [mainpage]
+ * 
+ *    responses:
+ *     200:
+ *      description: 정상처리
+ *      schema:
+ *       properties:
+ *        userId:
+ *         type: string
+ *        artistId:
+ *         type: string
+ *        photo:
+ *         type: string
+ *        groupName:
+ *         type: string,
+ *        
+ *     401:
+ *      description: 에러처리
  *      schema:
  *       properties:
  *        message:
