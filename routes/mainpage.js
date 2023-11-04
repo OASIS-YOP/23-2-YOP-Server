b024ae44d84e0e0f8c1b673d13517138f0803e7f
urimJ <ufrl128@dgu.ac.kr>
2023-11-04 22:00:07 +0900

    MOD api

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -33,33 +33,6 @@
  *         type: string
  *
  *
- * @swagger
- * paths:
- *  /mainpage:
- *   get:
- *    summary: 폴꾸 hot10
- *    tags: [mainpage]
- * 
- *    responses:
- *     200:
- *      description: 정상처리
- *      schema:
- *       properties:
- *        userId:
- *         type: string
- *        artistId:
- *         type: string
- *        photo:
- *         type: string
- *        groupName:
- *         type: string,
- *        
- *     401:
- *      description: 에러처리
- *      schema:
- *       properties:
- *        message:
- *         type: string
  *
  *
  * @swagger
