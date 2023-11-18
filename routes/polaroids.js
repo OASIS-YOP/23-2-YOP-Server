1d8c0d2a225b24ab6ecc818ba3972c69309fc741
urimJ <ufrl128@dgu.ac.kr>
2023-11-19 00:34:43 +0900

    MOD

--- routes/polaroids.js
+++ routes/polaroids.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import upload from '../modules/multer.js';
 import PolaroidController from '../controller/polaroids.js';
 
-router.post('/edit/save', upload.single('image'), PolaroidController.uploadImage);
+router.post('/polaroid', upload.single('image'), PolaroidController.uploadImage);
 
 export default router;
 
